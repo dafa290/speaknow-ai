c83c5070b81fee258299ed5ed30d57d7d252699c
muh daffa <dafanasuyion@gmail.com>
2026-07-20 23:05:11 +0700

    Add toString method to SessionRequest model

--- speaknow/src/main/java/com/speaknow/model/SessionRequest.java
+++ speaknow/src/main/java/com/speaknow/model/SessionRequest.java
@@ -19,4 +19,14 @@ public class SessionRequest {
 
     public Long getUserId() { return userId; }
     public void setUserId(Long userId) { this.userId = userId; }
+
+    @Override
+    public String toString() {
+        return "SessionRequest{" +
+                "question=" + question +
+                ", userAnswer=" + userAnswer +
+                ", mode=" + mode +
+                ", userId=" + userId +
+                '}';
+    }
 }
